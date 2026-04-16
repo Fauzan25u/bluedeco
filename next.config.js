78c0acd78626b30e0ab62d18b2950b372ebe4c2f
Fauzan25u <fauzanabdulai6@gmail.com>
2026-04-16 07:58:59 -1200

    Create next.config.js with Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig} 
+ **/
+const nextConfig = {
+    reactStrictMode: true,
+    // Additional configuration options go here
+};
+
+module.exports = nextConfig;
