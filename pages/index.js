59edfdf0464acdcace30f50c6d64b901259710ad
Fauzan25u <fauzanabdulai6@gmail.com>
2026-04-16 07:59:53 -1200

    Create pages/index.js with the BLUDECO homepage component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Welcome to BLUDECO</h1>
+            <p>Your one-stop solution for all things deco!</p>
+        </div>
+    );
+};
+
+export default Homepage;
